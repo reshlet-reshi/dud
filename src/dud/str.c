c2e384816cf5d8dd75fa8406484b5f5b487b7583
reshi-codex <285301776+reshi-codex@users.noreply.github.com>
2026-05-24 05:57:34 -0700

    ditch string.h

--- src/dud/str.c
+++ src/dud/str.c
@@ -1,11 +1,24 @@
-#include <string.h>
-
 #include "str.h"
 
 int dud_strcmp(char *s1, char *s2) {
-    return strcmp(s1, s2);
+    int i = 0;
+
+    while (s1[i] == s2[i]) {
+        if (s1[i] == '\0') {
+            return 0;
+        }
+        i = i + 1;
+    }
+
+    return ((unsigned char) s1[i]) - ((unsigned char) s2[i]);
 }
 
 int dud_strlen(char *s) {
-    return strlen(s);
+    int len = 0;
+
+    while (s[len] != '\0') {
+        len = len + 1;
+    }
+
+    return len;
 }
