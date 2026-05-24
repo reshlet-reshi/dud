fc08cf835e09f9f469ee657cb03c3040f7f7d4c4
reshi-codex <285301776+reshi-codex@users.noreply.github.com>
2026-05-23 17:16:22 -0700

    Add quot stream action

--- src/action.c
+++ src/action.c
@@ -3,10 +3,12 @@
 
 #include "action.h"
 #include "cat.h"
+#include "quot.h"
 #include "wc_c.h"
 
 fn_action *action_from_arg(char *arg) {
     if (strcmp(arg, "--cat") == 0) { return cat; }
+    if (strcmp(arg, "--quot") == 0) { return quot_bytes; }
     if (strcmp(arg, "--wc-c") == 0) { return wc_c; }
     return NULL;
 }
---
+++ src/quot.h
@@ -0,0 +1,8 @@
+#ifndef QUOT_H
+#define QUOT_H
+
+#include <stdio.h>
+
+void quot_bytes(FILE *input);
+
+#endif
