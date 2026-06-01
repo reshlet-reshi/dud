4b3968a9a8aeabdffb4743633585329988732075
reshi-codex <285301776+reshi-codex@users.noreply.github.com>
2026-05-31 19:19:09 -0700

    comments

--- runme.sh
+++ runme.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eu
 
+# usage/args check
+
 usage() {
     printf '%s\n' 'usage: ./runme.sh' >&2
 }
@@ -10,9 +12,19 @@ if [ "$#" -ne 0 ]; then
     exit 2
 fi
 
+# ensure ignored dir
+
 mkdir -p .dud
 
+# shellcheck all
+
 ./00-shellcheck/runme.sh
+
+# smoke-cc
+
 cp 01-smoke-cc/script.sh .dud/smoke-cc
 chmod 755 .dud/smoke-cc
+
+# musl-cc
+
 ./02-musl-cc/runme.sh
